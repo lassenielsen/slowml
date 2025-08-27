c864875a23005d54a58a6e1df67661a6277f7ab7
Lasse Nielsen <lasse.nielsen.dk@gmail.com>
2025-08-27 22:28:35 +0200

    Added demo and models to snake example

---
+++ examples/snake/rlsnake_demo.sh
@@ -0,0 +1,15 @@
+./rlsnake_test -m models/rlsnake_1_easy.mod     1 \
+               -m models/rlsnake_1_medium.mod   1 \
+               -m models/rlsnake_1_hard.mod     1 \
+               -m models/rlsnake_2_easy.mod     2 \
+               -m models/rlsnake_2_medium.mod   2 \
+               -m models/rlsnake_2_hard.mod     2 \
+               -m models/rlsnake_3_easy.mod     3 \
+               -m models/rlsnake_3_medium.mod   3 \
+               -m models/rlsnake_3_hard.mod     3 \
+               -m models/rlsnake_4_easy.mod     4 \
+               -m models/rlsnake_4_medium.mod   4 \
+               -m models/rlsnake_4_hard.mod     4 \
+               -m models/rlsnake_n8_easy.mod   -8 \
+               -m models/rlsnake_n8_medium.mod -8 \
+               -m models/rlsnake_n8_hard.mod   -8
